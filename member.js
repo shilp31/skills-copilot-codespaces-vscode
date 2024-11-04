808e81b6df67921209b6b540e9fc0fabe31d9c62
Shilpa <128037498+shilp31@users.noreply.github.com>
2024-11-04 13:48:20 +0000

    Added memberskills

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    skills: [
+      { name: 'javascript', level: 3 },
+      { name: 'python', level: 2 },
+      { name: 'ruby', level: 1 },
+    ],
+  };
+}
